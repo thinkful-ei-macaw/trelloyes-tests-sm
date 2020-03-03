198de29d1b753d18ac3385067590edb696eb3980
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-03 12:06:15 -0500

    Added snapshot test for List component

--- src/List.test.js
+++ src/List.test.js
@@ -6,16 +6,23 @@ import List from './List'
 
 describe ('<List />', () => {
 
+  const arr = [{ id: 'a', title: 'First card', content: 'lorem ipsum' },
+    { id: 'b', title: 'Second card', content: 'lorem ipsum' }];
+
   it('renders without crashing', () => {
     const div = document.createElement
     ('div');
 
-    const arr = [{ id: 'a', title: 'First card', content: 'lorem ipsum' },
-    { id: 'b', title: 'Second card', content: 'lorem ipsum' }]
-
     ReactDOM.render(<List header="head" cards={arr} />, div);
-
     ReactDOM.unmountComponentAtNode(div);
 
-  })
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List header="head" cards={arr}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot(); 
+  });
+
 })
